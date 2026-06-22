5ca7a6343a3f35e6bb874262608c43628c6a2d12
munafBCloud <munaf8289@gmail.com>
2026-06-22 03:17:25 +0000

    Deploy frontend to S3 static website hosting

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,6 +1,12 @@
 resource "aws_apigatewayv2_api" "itl_api" {
   name          = "itl-systems-api"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET", "POST", "OPTIONS"]
+    allow_headers = ["content-type"]
+  }
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
